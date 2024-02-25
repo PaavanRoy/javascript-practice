0a22a07cde32ec39927494f116c4af7f6a062c6e
PaavanRoy <102508482+PaavanRoy@users.noreply.github.com>
2024-02-25 12:55:20 +0000

    DataTypes

--- js-practice/dataTypes.js
+++ js-practice/dataTypes.js
@@ -3,3 +3,13 @@ console.log(roy);
 
 console.log(typeof roy);
 console.log(typeof null);
+
+// number
+// bigint
+// string
+//boolean
+// null // its is stand alone value 
+// undifined 
+//symbol
+// object
+
