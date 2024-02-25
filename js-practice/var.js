39282624af4bafe50d91925b74da2b054495ac54
PaavanRoy <102508482+PaavanRoy@users.noreply.github.com>
2024-02-25 05:16:16 +0000

    variable

---
+++ js-practice/var.js
@@ -0,0 +1,11 @@
+const accountid=123456
+let accountEmail="paavan@gmail.com"
+var accountPassword="786786"
+accountCity="Viazag"
+
+//accountid=7777
+accountEmail="roy"
+accountPassword="12347"
+
+
+console.table([accountEmail,accountPassword,accountid,accountCity])
