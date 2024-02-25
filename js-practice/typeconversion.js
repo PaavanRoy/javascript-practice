61fc4c9b417bc8dc3e6ed2cdabbe1502e84fa8d7
PaavanRoy <102508482+PaavanRoy@users.noreply.github.com>
2024-02-25 12:55:00 +0000

    DataTypes

---
+++ js-practice/typeconversion.js
@@ -0,0 +1,40 @@
+
+/*let score="33"
+console.log( typeof score);
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);*/
+
+/*let score=null
+console.log( typeof score);
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);*/ 
+
+// when you keep boolean it comes output as  0or1 
+// for undifeined its NaN
+// when u put string in and try to covert into number its output is NaN
+// when u convert 1 number to bollean its output is true same for false 
+// if its empty then its false
+// if its string its true
+
+
+/*************operations********/
+
+let value =3
+let newValue=-value
+//console.log(newValue);
+
+console.log(+true)
+// +true value is 1 
+/****************summary********************* */
+
+// primative data types 
+
+// 7 types : strings ,number, boolean ,null ,undefined,bigint,symbol
+
+// refrence (non-premative): array, object, functions
+
+
+
