bde06853138891dca8e80ec67c44bfcefbb56a40
PaavanRoy <102508482+PaavanRoy@users.noreply.github.com>
2024-02-26 16:13:59 +0000

    Nums and Math

---
+++ js-practice/nums_math.js
@@ -0,0 +1,20 @@
+//let balance = new Number(200)
+//console.log(balance.toString().length );
+//console.log(balance.toFixed(4)); // 200.0000
+
+//let anothernumber = 20.154515
+//console.log(anothernumber.toPrecision(2)); // 20
+
+//let hunderd= 100000000
+//console.log(hunderd.toLocaleString('en-IN')); //10,00,00,000
+
+
+/**********************maths***************** */
+
+let Number=200
+console.log(Math);
+console.log(Math.abs(-4)); // converts negitive to postive
+console.log(Math.round(4.4));//rounoff the Number
+console.log(Math.ceil(3.2));// chooses top number irrespective of round
+console.log(Math.floor(4.9));// chooses the lower value 
+console.log(Math.min(1,2,3,4,5,6,8));// prints min value same with max
