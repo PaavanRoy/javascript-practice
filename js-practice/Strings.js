b431629009c98967eedcb34c6d03c7cb04d53f45
PaavanRoy <102508482+PaavanRoy@users.noreply.github.com>
2024-02-26 16:14:07 +0000

    Strings

---
+++ js-practice/Strings.js
@@ -0,0 +1,10 @@
+let Names="paavan"
+let Count =26
+
+console.log(`My Name is ${Names} and my count is ${Count}`);
+
+console.log(Names[2]);
+console.log(Names.length);
+console.log(Names.toUpperCase());
+console.log(Names);
+console.log(Names.charAt(4));
